91436a7c0c84110c5400e354cf7911aabdc5c5c6
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-17 04:17:29 +0000

    Adds enumerable extensions

---
+++ Darker.Common.Tests/EnumerableExtensionsTest.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Darker.Test
+{
+    [TestFixture]
+    public class EnumerableExtensionsTest
+    {
+        [TestFixture]
+        public class ToDisplayString
+        {
+            [Test]
+            [TestCaseSource(nameof(_expectedPrintOutputCases))]
+            public void ExpectedPrintOutput(List<object> items, string expectedOutput)
+            {
+                var output = items.ToDisplayString();
+                Assert.AreEqual(expectedOutput, output);
+            }
+
+            private static object[] _expectedPrintOutputCases =
+            {
+                new object[] {new List<object> {1, 2, 3, 4}, "[1, 2, 3, 4]"},
+                new object[]
+                    {new List<object> {"Jason", "Tom", "Mary", "Carl", "Lenny"}, "[Jason, Tom, Mary, Carl, Lenny]"},
+                new object[] {new List<object> {"Dave"}, "[Dave]"},
+                new object[] {new List<object> {"Sarah", "Jenny"}, "[Sarah, Jenny]"},
+                new object[] {new List<object>(), "[]"}
+            };
+
+            [Test]
+            public void Null_Object_Does_Not_Throw()
+            {
+                List<object> list = null;
+
+                Assert.DoesNotThrow(() =>
+                {
+                    var output = list.ToDisplayString();
+                    Assert.AreEqual("null", output);
+                });
+            }
+
+            [Test]
+            public void Empty_List_Shows_Empty_Brackets()
+            {
+                var list = new List<object>();
+
+                var output = list.ToDisplayString();
+                Assert.AreEqual("[]", output);
+            }
+        }
+
+        [TestFixture]
+        public class AllElementsAreUnique
+        {
+            [Test]
+            public void NullEnumerable_Throws_NullArgument()
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                {    
+                  ((List<object>)null).AllElementsAreUnique();
+                });
+            }
+
+            [Test]
+            [TestCaseSource(nameof(_expectedDistinctOutputCases))]
+            public void When_All_Distinct_Return_True(List<object> items)
+            {
+                Assert.IsTrue(items.AllElementsAreUnique(), items.ToDisplayString());
+            }
+
+
+            private static object[] _expectedDistinctOutputCases =
+            {
+                new List<object> {1, 2, 3, 4},
+                new List<object> {"Jason", "Tom", "Mary", "Carl", "Lenny"},
+                new List<object> {"Dave"},
+                new List<object> {"Sarah", "Jenny"},
+                new List<object>()
+            };
+
+            [Test]
+            [TestCaseSource(nameof(_expectedNotDistinctOutputCases))]
+            public void When_All_Distinct_Return_False(List<object> items)
+            {
+                Assert.IsFalse(items.AllElementsAreUnique(), items.ToDisplayString());
+            }
+
+            private static object[] _expectedNotDistinctOutputCases =
+            {
+                new List<object> {1, 2, 3, 4,3},
+                new List<object> {"Jason", "Tom", "Tom", "Mary", "Carl", "Lenny"},
+                new List<object> {"Dave","Dave","Dave"},
+                new List<object> {"Sarah", "Jenny","Sarah"}
+            };
+        }
+
+        [TestFixture]
+        public class FilterByType
+        {
+            [Test]
+            [TestCaseSource(nameof(_expectedFilterByCases))]
+            public void Filter_Returns_Correct_Count(List<object> items,Type type,int amount)
+            {
+                var matching = items.FilterByType(type).ToList();
+
+                Assert.AreEqual(amount,matching.Count);
+            }
+
+            private static object[] _expectedFilterByCases =
+            {
+                new object[]
+                {
+                    new List<object> { "Jason", "Tom",1, 2, 3, 4}, typeof(string),2
+                },
+                new object[]
+                {
+                    new List<object> { "Jason", "Tom",1, 2, 3, 4}, typeof(int),4
+                },
+                new object[]
+                {
+                    new List<object>(),typeof(int),0 
+                }
+            };
+        }
+
+        [TestFixture]
+        public class FilterByTypeGeneric
+        {
+
+            [Test]
+            public void List_with_3_Ints_Filtering_Ints_returns_Three()
+            {
+                var items = new List<object> {"Jason", "Tom", 1, 2, 3};
+                var ints = items.FilterByType<object,int>();
+
+                Assert.AreEqual(3,ints.Count());
+            }
+
+            [Test]
+            public void List_with_2_Strings_Filtering_Ints_returns_Two()
+            {
+                var items = new List<object> { "Jason", "Tom", 1, 2, 3 };
+                var ints = items.FilterByType<object, string>();
+
+                Assert.AreEqual(2, ints.Count());
+            }
+
+        }
+
+        [TestFixture]
+        public class MaxBy
+        {
+
+            [Test]
+            public void Max_By_Name_Returns_Latest_Alphabet_Letter()
+            {
+                var items = GetTestData();
+
+                var chosen = items.MaxBy(x => x.Name);
+
+                Assert.AreEqual("Tom", chosen.Name);
+            }
+
+            [Test]
+            public void Max_By_Age_Returns_Highest_Number()
+            {
+                var items = GetTestData();
+
+                var chosen = items.MaxBy(x => x.Age);
+
+                Assert.AreEqual(41, chosen.Age);
+            }
+
+
+            List<SortTester> GetTestData()
+            {
+                return new List<SortTester>
+                {
+                    new SortTester { Name = "Tom", Age = 23 },
+                    new SortTester { Name = "Mary", Age = 31 },
+                    new SortTester { Name = "Max", Age = 30 },
+                    new SortTester { Name = "Sarah", Age = 24 },
+                    new SortTester { Name = "Gavin", Age = 17 },
+                    new SortTester { Name = "Lenny", Age = 35 },
+                    new SortTester { Name = "Olaf", Age = 41 },
+                    new SortTester { Name = "James", Age = 8 },
+                    new SortTester { Name = "Elenor", Age = 26 }
+                };
+            }
+        }
+
+        [TestFixture]
+        public class MinBy
+        {
+
+            [Test]
+            public void Min_By_Name_Returns_Earliest_Alphabet_Letter()
+            {
+                var items = GetTestData();
+
+                var chosen = items.MinBy(x => x.Name);
+
+                Assert.AreEqual("Elenor", chosen.Name);
+            }
+
+            [Test]
+            public void Max_By_Age_Returns_Highest_Number()
+            {
+                var items = GetTestData();
+
+                var chosen = items.MinBy(x => x.Age);
+
+                Assert.AreEqual(8, chosen.Age);
+            }
+
+
+            List<SortTester> GetTestData()
+            {
+                return new List<SortTester>
+                {
+                    new SortTester { Name = "Tom", Age = 23 },
+                    new SortTester { Name = "Mary", Age = 31 },
+                    new SortTester { Name = "Max", Age = 30 },
+                    new SortTester { Name = "Sarah", Age = 24 },
+                    new SortTester { Name = "Gavin", Age = 17 },
+                    new SortTester { Name = "Lenny", Age = 35 },
+                    new SortTester { Name = "Olaf", Age = 41 },
+                    new SortTester { Name = "James", Age = 8 },
+                    new SortTester { Name = "Elenor", Age = 26 }
+                };
+            }
+        }
+
+
+        public class SortTester
+        {
+            public string Name;
+            public int Age;
+        }
+
+    }
+}
---
+++ Darker.Common/EnumerableExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Darker
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        ///     Converts an enumerable into a string in the format: <c>'[A,B,C]'</c>
+        /// </summary>
+        /// <param name="source">Enumeration collection source</param>
+        /// <returns>Formatted String</returns>
+        public static string ToDisplayString(this IEnumerable source)
+        {
+            if (source == null) return "null";
+            var usableSource = source.Cast<object>().ToList();
+            if (!usableSource.Any()) return "[]";
+            var s = "[";
+            s = usableSource.Aggregate(s, (res, x) => res + x.ToString() + ", ");
+            return $"{s.Substring(0, s.Length - 2)}]";
+        }
+
+        /// <summary>
+        ///     Returns true if each element in the collection is distinct from each other
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns><c>true</c> if all elements unique, <c>false</c> otherwise.</returns>
+        public static bool AllElementsAreUnique<T>(this IEnumerable<T> source)
+        {
+            var enumerable = source as IList<T> ?? source.ToList();
+            if (!enumerable.Any()) return true;
+            return enumerable.Distinct().Count() == enumerable.Count;
+        }
+
+        /// <summary>
+        /// Filters an Enumerable collection by the class or interface subtype
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TFilter">The type of the t filter.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <returns>IEnumerable of the filtered type</returns>
+        public static IEnumerable<TFilter> FilterByType<T, TFilter>(this IEnumerable<T> source)
+            where T : class
+            where TFilter : T
+        {
+            return source.Where(item => item is TFilter).Cast<TFilter>();
+        }
+
+        /// <summary>
+        /// Filters an Enumerable collection by the class or interface subtype
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="filterType"></param>
+        /// <returns>IEnumerable of the filtered type</returns>
+        public static IEnumerable<T> FilterByType<T>(this IEnumerable<T> source,Type filterType)
+            where T : class
+        {
+            return source.Where(item => item.GetType() == filterType);
+        }
+
+        /// <summary>
+        ///     Returns highest element in collection by provided comparator
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="score">The score.</param>
+        /// <returns>T.</returns>
+        public static T MaxBy<T>(this IEnumerable<T> source, Func<T, IComparable> score)
+        {
+            return source.Aggregate((x, y) => score(x).CompareTo(score(y)) > 0 ? x : y);
+        }
+
+        /// <summary>
+        ///     Returns lowest element in collection by provided comparator
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="score">The score.</param>
+        /// <returns>T.</returns>
+        public static T MinBy<T>(this IEnumerable<T> source, Func<T, IComparable> score)
+        {
+            return source.Aggregate((x, y) => score(x).CompareTo(score(y)) < 0 ? x : y);
+        }
+
+    }
+}
