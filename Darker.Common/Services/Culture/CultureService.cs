8f858f31aa964937cf0a0a06da912c6396a92e40
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-17 06:26:06 +0000

    Adds private constructor to remove the ability to instanciate singleton

--- Darker.Common/Services/Culture/CultureService.cs
+++ Darker.Common/Services/Culture/CultureService.cs
@@ -6,6 +6,10 @@ namespace Darker
     {
         private static CultureService _instance;
 
+        private CultureService()
+        {
+        }
+
         public static CultureService Instance => _instance ?? (_instance = new CultureService());
         public CultureInfo CurrentCulture => CultureInfo.CurrentCulture;
     }
